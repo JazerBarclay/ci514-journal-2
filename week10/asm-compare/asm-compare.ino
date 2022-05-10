0c8c80f9c70cc462afbb067695e35643f84325c0
Jazer <contact@jazer.co.uk>
2022-05-10 21:03:20 +0100

    Added example compare sketch

---
+++ week10/asm-compare/asm-compare.ino
@@ -0,0 +1,15 @@
+/* 
+ * Assembly Compare
+ * 
+ * Compare numbers to see which is bigger
+ * and turns of the on-board led if
+ * the first is bigger than the second
+ * 
+ * If the values are equal then the led
+ * will remain off.
+ * 
+ * The code shows where to add an equal 
+ * operation could be added.
+ * 
+ * Author: Jazer
+ */
