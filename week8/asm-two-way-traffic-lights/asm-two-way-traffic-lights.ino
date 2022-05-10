6a6b3f2a5976f2d503b5ebebd1a6e60794a7bc16
Jazer <contact@jazer.co.uk>
2022-05-10 21:01:13 +0100

    Added two way traffic lights sketch

---
+++ week8/asm-two-way-traffic-lights/asm-two-way-traffic-lights.ino
@@ -0,0 +1,13 @@
+/* 
+ * Assembly Two Way Traffic Lights
+ * 
+ * Like the timer overflow interrupt we set
+ * the counter and decrement it every interrupt.
+ * 
+ * When the counter hits 0 we have the zero
+ * flag set. With this we can execute some 
+ * subroutine that happens after a certain 
+ * number of interrupts.
+ * 
+ * Author: Jazer Barclay
+ */
