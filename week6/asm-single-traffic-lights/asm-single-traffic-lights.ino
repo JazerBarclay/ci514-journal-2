963aed1876724faecf6b87987779571edb9e4325
Jazer <contact@jazer.co.uk>
2022-05-10 20:59:25 +0100

    Added single traffic light sketch

---
+++ week6/asm-single-traffic-lights/asm-single-traffic-lights.ino
@@ -0,0 +1,9 @@
+/* 
+ * Assembly Single Traffic Lights
+ * 
+ * The sketch cycles through the 4 phases a single
+ * set of traffic lights go through with no pedestrian
+ * crossing inputs yet.
+ * 
+ * Author: Jazer Barclay
+ */
