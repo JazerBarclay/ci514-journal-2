50942d3c122ef242428f22d165925806341854a6
Jazer <contact@jazer.co.uk>
2022-05-10 21:02:27 +0100

    Added serial example sketch

---
+++ week9/asm-serial/asm-serial.ino
@@ -0,0 +1,14 @@
+/* 
+ * Assembly Serial
+ * 
+ * I created a simple sketch that uses the 5 i/o registers
+ * associated with serial communication to transmit the
+ * character 'A' (ASCII 65) with a 1 second delay between
+ * each tramission.
+ * 
+ * UDR - USART Data Register
+ * UCSR[A,B,C] USART Control Status Register
+ * UBRR - USART Baud Rate Register
+ * 
+ * Author: Jazer
+ */
