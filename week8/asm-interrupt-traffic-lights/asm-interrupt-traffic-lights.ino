5c9c1bfb30b60b7a5951393ca0ae127af17b77d6
Jazer <contact@jazer.co.uk>
2022-05-10 21:00:57 +0100

    Added interrupt driven two way traffic lights sketch

---
+++ week8/asm-interrupt-traffic-lights/asm-interrupt-traffic-lights.ino
@@ -0,0 +1,12 @@
+/* 
+ * Assembly Interrupt Driven Single Traffic Lights
+ * 
+ * This sketch is written in pure assembly in the
+ * associated "asm-single-traffic-lights.S" file.
+ * 
+ * The sketch cycles through the 4 phases a single
+ * set of traffic lights go through with no pedestrian
+ * crossing inputs yet on a delay of 10 seconds.
+ * 
+ * Author: Jazer Barclay
+ */
